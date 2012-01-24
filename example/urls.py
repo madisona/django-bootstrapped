a1a09bd744fbc3766da0f1f0f7b28d4094efa7d1
Percy <aaron.madison@theimtgroup.com>
2012-01-24 10:47:15 -0600

    added example templates

--- example/urls.py
+++ example/urls.py
@@ -4,6 +4,17 @@ from django.conf.urls.defaults import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
+from django.views import generic
+
+class Example1(generic.TemplateView):
+    template_name = "examples/example_1.html"
+
+class Example2(generic.TemplateView):
+    template_name = "examples/example_2.html"
+
+class Example3(generic.TemplateView):
+    template_name = "examples/example_3.html"
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'example.views.home', name='home'),
@@ -14,4 +25,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^example1/$', Example1.as_view()),
+    url(r'^example2/$', Example2.as_view()),
+    url(r'^example3/$', Example3.as_view()),
 )
